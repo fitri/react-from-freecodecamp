9454559be9014bb99c5bc1b6bac62e7e49012c97
fitri <fitri@fitri.me>
2023-02-18 14:23:30 +0800

    this is how to put multiple element, all must be encapsule inside of one element for exmpample in this case h1 and ul put inside one div only

--- Chapter_01_Static_Page_with_React/Simple_Example/index.js
+++ Chapter_01_Static_Page_with_React/Simple_Example/index.js
@@ -1,2 +1,13 @@
 // ReactDOM(html to render, location to render)
-ReactDOM.render(<h1>Ohayou!</h1>, document.getElementById("root"));
+ReactDOM.render(
+	<div>
+	<h1>Lesson Notes</h1>
+		<ul>
+			<li>Import react from the CDN</li>		
+			<li>Import babel and overwrite the script to type text/babel</li>
+			<li>Create a root element div id tag to place react element</li>
+			<li>Using React.dom find specify element and id to place the element</li>
+		</ul>
+	</div>,
+	document.getElementById("root")
+);
