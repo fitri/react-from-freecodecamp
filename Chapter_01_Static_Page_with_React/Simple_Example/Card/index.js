fa7bd2edeb439f9472182049e530acb50e679e17
fitri <fitri@fitri.me>
2023-02-18 16:05:35 +0800

    test - create from sratach a card with the following example from the video tutorial

---
+++ Chapter_01_Static_Page_with_React/Simple_Example/Card/index.js
@@ -0,0 +1,19 @@
+function Body(){
+	return (
+		<div>
+			<h1>Fun Facts about React</h1>
+			<ul>
+				<li>Was released in 2013</li>
+				<li>Was originally created by Jordan Walke</li>
+				<li>Has over 100K star on Github</li>
+				<li>Is maintained by Facebook</li>
+				<li>Powered thousand of enterprise apps including mobile apps</li>
+			</ul>
+		</div>
+		)
+}
+
+ReactDOM.render(
+	<Body />,
+	document.getElementById("root")
+)
