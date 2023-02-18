de47586721395e9a75929f246b1e53f8b3276c66
fitri <fitri@fitri.me>
2023-02-18 14:43:24 +0800

    writing reusable react component with function and call it with custom react tag

--- Chapter_01_Static_Page_with_React/Simple_Example/index.js
+++ Chapter_01_Static_Page_with_React/Simple_Example/index.js
@@ -1,13 +1,22 @@
-// ReactDOM(html to render, location to render)
-ReactDOM.render(
-	<div>
-	<h1>Lesson Notes</h1>
-		<ul>
+// Using function to group element for reusable, write a function with name and return the html element
+// React function use className not class as the normal html
+function Listing() {
+	return(
+		<ul className="list lesson">
 			<li>Import react from the CDN</li>		
 			<li>Import babel and overwrite the script to type text/babel</li>
 			<li>Create a root element div id tag to place react element</li>
 			<li>Using React.dom find specify element and id to place the element</li>
 		</ul>
+		)
+}
+
+// ReactDOM(html to render, location to render)
+// Now we can call the above newly reacted react component with custom react tag <Listing/>
+ReactDOM.render(
+	<div>
+	<h1>Lesson Notes</h1>
+		<Listing/>
 	</div>,
 	document.getElementById("root")
 );
