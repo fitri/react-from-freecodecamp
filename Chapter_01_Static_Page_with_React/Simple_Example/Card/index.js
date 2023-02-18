534b99e5f6787e0ea2136a2984db91e62f49bc87
fitri <fitri@fitri.me>
2023-02-19 00:12:18 +0800

    add ul as navigation

--- Chapter_01_Static_Page_with_React/Simple_Example/Card/index.js
+++ Chapter_01_Static_Page_with_React/Simple_Example/Card/index.js
@@ -2,20 +2,29 @@ function Body(){
 	return (
 		<div>
 			<h1>Fun Facts about React</h1>
-			<ul>
+			<ol>
 				<li>Was released in 2013</li>
 				<li>Was originally created by Jordan Walke</li>
 				<li>Has over 100K star on Github</li>
 				<li>Is maintained by Facebook</li>
 				<li>Powered thousand of enterprise apps including mobile apps</li>
-			</ul>
+			</ol>
 		</div>
 		)
 }
 
 function Header(){
 	return (
+		<>
 		<header><img src="./reactlogo.png" width="60px"/></header>
+		<nav>
+			<ul>
+				<li>About</li>
+				<li>Pricing</li>
+				<li>More</li>
+			</ul>
+		</nav>
+		</>
 		)
 }
 
